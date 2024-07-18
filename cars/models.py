c2bd45e4fe5498db91f84f6252ea27791fc2c88f
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-07-18 13:31:43 -0300

    add: foto padrão para carros adicionados sem foto

--- cars/models.py
+++ cars/models.py
@@ -17,7 +17,7 @@ class Car(models.Model):
     model_year = models.IntegerField(blank=True, null=True)
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
-    photo = models.ImageField(upload_to='cars/', blank=False)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
