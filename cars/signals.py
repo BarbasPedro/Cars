6d86c76bfd34f233a2faf7006cae25e81e043405
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-07-18 12:49:08 -0300

    feat: Integrando API da OpenAI criando o scripts para geração de bio

--- cars/signals.py
+++ cars/signals.py
@@ -1,8 +1,9 @@
-from django.db.models.signals import post_delete, post_save
 from django.db.models import Sum
+from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
 
 from cars.models import Car, CarInventory
+from openai_api.client import get_car_ai_bio
 
 
 def car_inventory_update():
@@ -14,6 +15,22 @@ def car_inventory_update():
       cars_count = cars_count,
       cars_value = cars_value
   )
+
+
+
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+  if not instance.bio:
+    ai_bio = get_car_ai_bio(
+      instance.model, instance.brand, instance.model_year
+    )
+    instance.bio = ai_bio
+
+
+
+
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
